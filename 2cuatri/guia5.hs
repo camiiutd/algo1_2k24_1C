9142bed9b006dfd7f24f40ee4a78f0b517225362
cami3p <72765897+camiiutd@users.noreply.github.com>
2024-09-21 00:01:02 -0300

    Add files via upload

--- 2cuatri/guia5.hs
+++ 2cuatri/guia5.hs
@@ -136,3 +136,169 @@ multiplosDeN n (x:xs) | mod x n ==0= x : multiplosDeN n xs
 ordenar :: [Integer] -> [Integer] 
 ordenar [] = []
 ordenar xs = maximo xs : ordenar (quitar (maximo xs) xs)
+
+--4
+type Texto = [Char]
+--a
+sacarBlancosRepetidos :: Texto->Texto
+sacarBlancosRepetidos [] = []
+sacarBlancosRepetidos [x] = [x]
+sacarBlancosRepetidos (x:y:xs) | x == ' ' && y ==' ' = sacarBlancosRepetidos (y:xs)
+                               | otherwise= x: sacarBlancosRepetidos (y:xs)
+
+--b
+contarPalabras :: Texto -> Integer 
+contarPalabras [] =0
+contarPalabras [x] =1
+contarPalabras (x:xs) | x == ' ' = 1 + contarPalabras xs
+                      | otherwise= contarPalabras xs 
+
+--c cortesía d chatgpt
+palabras :: Texto -> [Texto]
+palabras [] = []
+palabras (x:xs) = auxPalabras (x:xs) []
+
+auxPalabras :: Texto->Texto -> [Texto]
+auxPalabras [] [] = []
+auxPalabras [] n = [n]
+auxPalabras (x:xs) n |x ==' ' && n ==[] = auxPalabras xs n
+                     | x== ' ' = n : auxPalabras xs []
+                     | otherwise= auxPalabras xs (n ++ [x])
+                    
+--d
+palabraMasLarga :: Texto-> Texto
+palabraMasLarga [] =[]
+palabraMasLarga [x] =[x]
+palabraMasLarga (x:xs) = maximoPalabras (palabras (x:xs))
+
+maximoPalabras :: [Texto] -> Texto
+maximoPalabras [] =[]
+maximoPalabras (x:xs) | longitud x > longitud (maximoPalabras xs) = x
+                      | otherwise= maximoPalabras xs
+
+
+--e
+aplanar :: [Texto] ->Texto
+aplanar [] = []
+aplanar (x:xs) = x ++ aplanar xs
+
+--f
+aplanarConBlancos ::[Texto] -> Texto
+aplanarConBlancos [] = []
+aplanarConBlancos (x:xs) = x ++ ' ' : aplanarConBlancos xs
+
+--g
+aplanarConNBlancos :: [Texto] -> Integer -> Texto
+aplanarConNBlancos [] _ = []
+aplanarConNBlancos [x] _ = x
+aplanarConNBlancos (x:xs) n = x ++ multiplicoEspacios n ++ aplanarConNBlancos xs n
+
+multiplicoEspacios :: Integer -> Texto
+multiplicoEspacios 0 = []
+multiplicoEspacios n =' ' : multiplicoEspacios (n-1)
+
+--5.1
+sumaAcumulada :: (Num t) => [t] -> [t]
+sumaAcumulada [] = []
+sumaAcumulada (x:xs) = sumaAcuAux xs x [x]
+
+sumaAcuAux :: (Num t) => [t] -> t -> [t] -> [t]
+sumaAcuAux [] _ [] = []
+sumaAcuAux [] _ ns = reverso ns
+sumaAcuAux (x:xs) y ns =sumaAcuAux xs (y+x) ((y+x ):ns)
+
+--5.2
+descomponerEnPrimos :: [Integer] -> [[Integer]]
+descomponerEnPrimos xs = descomponerPrimosAux xs
+
+descomponerPrimosAux :: [Integer] ->[[Integer]]
+descomponerPrimosAux []= []
+descomponerPrimosAux (x:xs) = divisoresDelNum x 2 : descomponerPrimosAux xs 
+
+divisoresDelNum :: Integer -> Integer -> [Integer]
+divisoresDelNum x n | n > x= []
+                    | mod x n ==0 && esPrimo n = n : divisoresDelNum x (n+1) 
+                    | otherwise= divisoresDelNum x (n+1)
+                    
+menorDivisor :: Integer->Integer
+menorDivisor n = menorDivAux n 2
+
+menorDivAux ::Integer->Integer ->Integer
+menorDivAux n k | mod n k == 0 = k
+                | n == k = n
+                | otherwise= menorDivAux n (k+1)
+
+esPrimo :: Integer -> Bool
+esPrimo n = menorDivisor n == n 
+--6a
+
+type Nombre = Texto
+type Telefono = Texto
+type Contacto = (Nombre, Telefono)
+type ContactosTel = [Contacto]
+
+enLosContactos :: Nombre -> ContactosTel ->Bool
+enLosContactos n c = enLosContactosAux n c
+
+enLosContactosAux :: [Char] -> [([Char], [Char])] ->Bool
+enLosContactosAux _ [] = False
+enLosContactosAux n ((a,b):ys) | n == a = True
+                               | otherwise= enLosContactosAux n ys
+
+--6b
+agregarContacto :: Contacto ->ContactosTel ->ContactosTel
+agregarContacto c ct = agregarContactoAux c ct
+
+agregarContactoAux :: ([Char], [Char]) -> [([Char], [Char])] -> [([Char], [Char])]
+agregarContactoAux _ [] = []
+agregarContactoAux (a,b) tel | enLosContactos a tel == True =  (a,b) : eliminoTupla (a,b) tel
+                             | otherwise= (a,b) : tel
+
+eliminoTupla :: ([Char], [Char]) -> [([Char], [Char])] -> [([Char], [Char])]
+eliminoTupla _ [] =[]
+eliminoTupla (a,b) ((c,d):ys) | a == c = ys
+                              | otherwise= (c,d) : eliminoTupla (a,b) ys
+
+--6c
+--(lo hice arriba mas o menos es lo mismo :vvvvvvvvvvvvvvvvv)
+eliminarContacto :: [Char] -> [([Char], [Char])] -> [([Char], [Char])]
+eliminarContacto _ [] = []
+eliminarContacto n ((a,b):xs) | a == n = xs 
+                              | otherwise= (a,b) : eliminarContacto n xs
+
+--7
+type Identificacion = Integer
+type Ubicacion = Texto --STRING 
+type Estado = (Disponibilidad, Ubicacion)
+type Locker = (Identificacion, Estado)
+type MapaDeLockers = [Locker]
+type Disponibilidad = Bool
+
+existeElLocker :: Integer ->[(Integer, (Bool, [Char]))] ->Bool
+existeElLocker n ((a,b):xs) | n == a = True
+                            | otherwise= existeElLocker n xs 
+
+--7.2
+ubicacionDelLocker :: Integer -> [(Integer, (Bool, [Char]))] ->[Char]
+ubicacionDelLocker _ [] = []
+ubicacionDelLocker n ((a,b):xs) | n == a = (snd b )
+                                | otherwise= ubicacionDelLocker n xs
+
+--7.3
+estaDisponibleElLocker :: Integer -> [(Integer, (Bool, [Char]))] ->Bool
+estaDisponibleElLocker n ((a,b):xs) | n == a = (fst b)
+                                    | otherwise= estaDisponibleElLocker n xs
+
+--7.4  
+ocuparLocker :: Integer -> [(Integer, (Bool, [Char]))]->[(Integer, (Bool, [Char]))]
+ocuparLocker n ((a,(b,c)):xs) |n==a && b ==False = (a,(True,c)) : xs
+                              | n==a&& b==True=((a,(b,c)):xs)
+                              | otherwise= (a,(b,c)) :ocuparLocker n xs
+
+
+eliminaLocker :: Integer -> [(Integer, (Bool, [Char]))]->[(Integer, (Bool, [Char]))] 
+eliminaLocker _ [] =[]
+eliminaLocker n ((a,b):xs) | n ==a = xs
+                           | otherwise= (a,b) : eliminaLocker n xs 
+
+
