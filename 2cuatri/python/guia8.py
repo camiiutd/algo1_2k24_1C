161194bcff64f8764d83b9606b7cc96ff85fd706
cami3p <72765897+camiiutd@users.noreply.github.com>
2024-10-29 01:54:54 -0300

    Add files via upload

--- 2cuatri/python/guia8.py
+++ 2cuatri/python/guia8.py
@@ -11,15 +11,29 @@ def generar_nros_al_azar(cantidad:int,desde:int,hasta:int)->Pila[int]:
         cantidad-=1
     return p
 
+def reversecasero(l:list)->list:
+    lista=[]
+    for e in range(len(l)-1,-1,-1):
+        lista.append(l[e])
+    return lista    
+
+
 def cantidad_elementos(p:Pila)->int:
     contador:int=0
+    l=[]
     p_aux=Pila()
     while not p.empty():
-        p_aux.put(p.get())
+        a=p.get()
+        p_aux.put(a)
+        l.append(a)
         contador+=1
-    while not p_aux.empty():
-        p.put(p_aux.get())
+
+    r=reversecasero(l)
+    for e in r:
+        p.put(e)
+
     return contador
+
 
 def buscar_el_maximo(p:Pila[int])->int:
     p_aux:Pila=Pila()
@@ -29,11 +43,11 @@ def buscar_el_maximo(p:Pila[int])->int:
         lista.append(p.get())
 
     for e in lista:
-        if e >= maximo:
+        if e > maximo:
             maximo=e
     
     while not p_aux.empty():
-        p.get(lista.pop(len(lista)-1))
+        p.get(reversecasero(lista).pop(len(lista)-1))
 
     return maximo
 
@@ -163,4 +177,125 @@ def generar_nros_al_azar(cantidad:int,desde:int,hasta:int)->Cola[int]:
         cantidad-=1
     return c
 
+
+
 c=generar_nros_al_azar(5,1,10)
+
+#9
+def cantidad_elementos(c:Cola)->int:
+    caux:Cola=Cola()
+    contador:int=0
+    while not c.empty():
+        caux.put(c.get())
+        contador+=1
+    
+    while not caux.empty():
+        c.put(caux.get())
+
+    return contador
+
+
+
+
+# #10
+def buscar_el_maximo(c:Cola[int])->int:
+    c_aux:Cola=Cola()
+    l:list[int]=[]
+    while not c.empty():
+        a=c.get()
+        c_aux.put(a)
+        l.append(a)
+    
+    maximo=l[0]
+    for e in l:
+        if e>maximo:
+            maximo=e
+    
+    while not c_aux.empty():
+        c.put(c_aux.get())
+
+    return maximo
+
+#11
+
+def buscar_nota_minima(c:Cola[str,int])->int:
+    caux:Cola=Cola()
+    l:list=[]
+
+    while not c.empty():
+        a=c.get()
+        l.append(a)
+        caux.put(a)
+    
+    m=l[0]
+    for e in l:
+        if e[1] < m[1]:
+            m=e
+    
+    while not caux.empty():
+        c.put(caux.get())
+    return m
+
+#12
+def intercalar(c1:Cola,c2:Cola)->Cola:
+    c1aux:Cola=Cola()
+    c2aux:Cola=Cola()
+    res:Cola=Cola()
+    
+    while not c1.empty() and not c2.empty():
+        a=c1.get()
+        c1aux.put(a)
+        res.put(a)
+        b=c2.get()
+        c2aux.put(b)
+        res.put(b)
+    
+    while not c1aux.empty():
+        c1.put(c1aux.get())
+    while not c2aux.empty():
+        c2.put(c2aux.get())
+    
+    return res 
+
+#13
+#1
+def armar_carton_de_bingo()->Cola[int]:
+    res=generar_nros_al_azar(12,0,99)
+    return res
+
+#2
+def pertint(a:list[int],n:int)->bool:
+    for e in a:
+        if e==n:
+            return True
+        
+    return False
+
+
+def jugar_carton_de_bingo(carton:list[int],bolillero:Cola[int])->int:
+    b_aux:Cola[int]=Cola()
+    contador:int=0
+    while not bolillero.empty():
+        a=bolillero.get()
+        b_aux.put(a)
+        contador+=1
+        if pertint(carton,a):
+            carton.remove(a)
+        
+    while not b_aux.empty():
+        bolillero.put(b_aux.get())
+    
+    return contador
+
+
+
+
+
+
+c1=Cola()
+c1.put(1)
+c1.put(2)
+c2=Cola()
+c2.put(4)
+c2.put(3)
+tr=intercalar(c1,c2)
