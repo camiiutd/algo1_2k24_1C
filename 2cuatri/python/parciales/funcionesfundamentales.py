0c998f6ea12685b953fd859fe432c3bae6862590
cami3p <72765897+camiiutd@users.noreply.github.com>
2024-11-25 16:03:55 -0300

    Actualizar funcionesfundamentales.py

--- 2cuatri/python/parciales/funcionesfundamentales.py
+++ 2cuatri/python/parciales/funcionesfundamentales.py
@@ -8,3 +8,21 @@ def matriz_traspuesta(m:list[list[int]])->list[list[int]]:
     return res
 
 
+def lista_de_maximos(l: list[int]) -> list[tuple[int, int]]:
+    res: list[tuple[int, int]] = []
+    inicio: int = -1  # Valor especial que indica "sin racha activa"
+
+    for i in range(len(l)):
+        if 0 < l[i] <= 61:  # Tiempo válido
+            if inicio == -1:  # Iniciar una nueva racha
+                inicio = i
+        else:
+            if inicio != -1:  # Fin de la racha
+                res.append((inicio, i - 1))
+                inicio = -1  # Reiniciar para indicar "sin racha activa"
+
+    # Verificar si al final de la lista hay una racha válida
+    if inicio != -1:
+        res.append((inicio, len(l) - 1))
+
+    return res
